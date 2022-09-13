2e73aa78fe7ba5c00b37e0d62864d81af9538eea
wavezM5 <102171820+wavezM5@users.noreply.github.com>
2022-09-13 09:34:51 +0300

    test123

--- Syntax, Functions and Statements - Lab/test.js
+++ Syntax, Functions and Statements - Lab/test.js
@@ -1,1 +1,11 @@
-console.log(1234);
+function solve(first,second,third){
+    let firstLenght=first.l;
+    let secondLenght=second.lenght;
+    let thirdLenght=third.lenght;
+
+    let sum=firstLenght+secondLenght+thirdLenght;
+    let result=Math.round(sum/3);
+    console.log(result);
+}
+
+solve('A','B','CCC');
