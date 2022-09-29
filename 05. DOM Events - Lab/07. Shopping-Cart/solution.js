3e9d882875c36faab4876d824434cecf6d1bb7e8
wavezM5 <102171820+wavezM5@users.noreply.github.com>
2022-09-29 20:38:17 +0300

    awesome edit

--- 05. DOM Events - Lab/07. Shopping-Cart/solution.js
+++ 05. DOM Events - Lab/07. Shopping-Cart/solution.js
@@ -1,58 +1,46 @@
-function solve() {
+function solve() { //my solution :) better one ;p
   
-    let shoppingCart = document.getElementsByClassName('shopping-cart')[0];
-    shoppingCart.addEventListener('click', onClick);
+    let textArea = document.getElementsByTagName('textarea')[0];
 
-    let result = document.getElementsByTagName('textarea')[0];
+    let addButtons = Array.from(document.getElementsByClassName('add-product'));
+    addButtons.forEach(x => {
+        x.addEventListener('click', add);
+    });
 
-    let products = [];
+    let resultProductsList = [];
     let totalPrice = 0;
+    let isCheckoutDone = false;
 
-    let checkoutDone = false;
-
-    function onClick(event) {
-
-        if(event.target.nodeName !== 'BUTTON') { //event.target.nodeName ALWAYS RETURN UPPERCASE
-             return;
-        }
-
-        if(checkoutDone) {
+    function add(event) {
+        if(isCheckoutDone) {
             return;
         }
 
-        let currButton = event.target;
-        let buttonClassList = Array.from(currButton.classList);
+        let parent = event.target.parentElement.parentElement;
+        let name = parent.querySelector('.product-title').textContent;
+        let price = parent.querySelector('.product-line-price').textContent;
 
-        if(buttonClassList.indexOf('add-product') >= 0) { 
-            let productElement = event.target.parentElement.parentElement;
-
-            let productName = productElement.querySelectorAll('.product-title')[0].textContent;
-            let productPrice = productElement.querySelectorAll('.product-line-price')[0].textContent;
-    
-            result.textContent += `Added ${productName} for ${productPrice} to the cart.\n`;
-    
-            if(products.indexOf(productName) < 0) {
-                products.push(productName);
-            }
-    
-            totalPrice += Number(productPrice);
-    
-            console.log(products);
-            console.log(totalPrice.toFixed(2));
-        }
-        else if(buttonClassList.indexOf('checkout') >= 0) {
-        
-            let list = products.join(', ');
-            result.textContent += `You bought ${list} for ${totalPrice.toFixed(2)}.`;
-
-            checkoutDone = true;
+        if(resultProductsList.indexOf(name) == -1) {
+            resultProductsList.push(name);
         }
         
+        totalPrice += Number(price);
 
-    };
+        textArea.textContent += `Added ${name} for ${price} to the cart.\n`;
+    }
 
+    let checkoutButton = document.getElementsByClassName('checkout')[0];
+    checkoutButton.addEventListener('click', checkout);
 
+    function checkout(event) {
+        if(isCheckoutDone) {
+            return;
+        }
 
-
+        let list = resultProductsList.join(', ');
+        textArea.textContent += `You bought ${list} for ${totalPrice.toFixed(2)}.`;
+        
+        isCheckoutDone = true;
+    }
    
 }
